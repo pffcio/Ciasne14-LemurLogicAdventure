0ccaf81326ac407e38409d9af525a596e360447b
LAPTOP-LENOVO\pawel <pawelek.tokarczyk@gmail.com>
2024-09-29 01:27:46 +0200

    dd

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,11 +2,6 @@ extends Node
 
 signal task_complited(succedded);
 
-var max_correct_answers_true_false = 3
-var correct_answers_true_false = 0
-var max_correct_answers_connectors = 3
-var correct_answers_connectors = 0
-
 func _ready():
 	task_complited.connect(_on_task_completed)
 	
