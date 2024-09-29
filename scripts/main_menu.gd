81e3bdb8731cd7042ebd5ca2663ad7df8b121761
LAPTOP-LENOVO\pawel <pawelek.tokarczyk@gmail.com>
2024-09-29 07:39:21 +0200

    ui fix

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var first_level: PackedScene
-@export var basic_logic_level: PackedScene
+@export var basic_logic_level = "res://scenes/basic_logic/lemur_tutorial_1_3.tscn"
 
 func _on_start_pressed():
 	$Start2.hide()
@@ -13,7 +13,7 @@ func _on_quit_pressed():
 
 
 func _on_introduction_pressed():
-	get_tree().change_scene_to_packed(basic_logic_level)
+	get_tree().change_scene_to_file(basic_logic_level)
 
 
 func _on_through_game_pressed():
